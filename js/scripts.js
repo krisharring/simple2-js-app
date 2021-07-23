dfcf1e1bd94df60b2285b6a9aaa7dd92992050a3
Kris <73711936+krisharring@users.noreply.github.com>
2021-07-23 11:54:04 -0400

    create PokemonList array

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,21 @@
-
+//Pokemen Array List
+let PokemenList = [
+  {
+    name: 'Genesect',
+    height: 1.5,
+    weight: 82.5,
+    abilities: ['download'],
+  }
+  {
+    name: 'Shuckle',
+    height: 0.6,
+    weight: 20.5,
+    abilities: ['Sturdy', 'Gluttony', 'Contrary'],
+  }
+  {
+    name: 'Loudred',
+    height: 1,
+    weight: 40.5,
+    abilities: ['Soundproof', 'Scrappy'],
+  }
+];
