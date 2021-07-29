c6e0c3bb3533dcd5d8b6bccfb1788c188a3454c9
Kris <73711936+krisharring@users.noreply.github.com>
2021-07-29 23:14:20 +0000

    added a conditional to the loop

--- js/scripts.js
+++ js/scripts.js
@@ -29,8 +29,17 @@ let PokemenList = [
 
 //Use what you’ve learned about adding strings in JavaScript to write the Pokémon’s height next to its name, for example, “Bulbasaur (height: 7)”.
 
-for (let i = 0; i < PokemenList.length; i++){
-		document.write(`<p> "${PokemenList[i].name} (height: ${PokemenList[i].height})" </p>`);
-	}
+//for (let i = 0; i < PokemenList.length; i++){
+//		document.write(`<p> "${PokemenList[i].name} (height: ${PokemenList[i].height})" </p>`);
+//	}
 
+//Next, add code to highlight special Pokémon in your list. Be sure to keep adding comments to explain and document what your code does. Within the loop, add a conditional. The conditional should check if the height is above a certain value (you’re free to pick whatever value you want). If it is, add the note “Wow, that’s big!” to the output. Make sure you set up the conditional so that only one Pokémon has the label “Wow, that’s big!” It could, for example, look like this: “Bulbasaur (height: 7) - Wow, that’s big!”. For example, if you had the following array:
 
+for (let i=0; i < PokemenList.length; i++){
+  if (PokemenList[i].height > 1) {
+    document.write(`<p> "${PokemenList[i].name} (height: ${PokemenList[i].height})" <b> I am the Tallest Pokemen!</b> </p>`)
+  }
+  else {
+  document.write(`<p> "${PokemenList[i].name} (height: ${PokemenList[i].height})" </p>`);
+  }
+}
