e6ce98fb6ce3114be2e758847ebf3026e8c165e1
Kris <73711936+krisharring@users.noreply.github.com>
2021-08-28 18:55:23 -0400

    updated index w pokedex image

--- js/scripts.js
+++ js/scripts.js
@@ -17,33 +17,7 @@ let pokemonRepository = (function (){
     pokemonImage.classList.add('Pokemon-image');
 
   
-  let pokemonList = [
-  // {
-  //     name: 'Braviary',
-  //     height: 1.5,
-  //     type: ['normal', 'flying']
-  // },
-  // {
-  //     name: 'Ho-oh',
-  //     height: 3.8,
-  //     type: ['flying', 'fairy', 'fire', 'grass']
-  // },
-  // {
-  //     name: 'Piplup',
-  //     height: 0.4,
-  //     type: ['water', 'ice']
-  // },
-  // {
-  //     name: 'Rapidash',
-  //     height: 1.7,
-  //     type: ['fire', 'ice']
-  // },
-  // {
-  //     name: 'Arbok',
-  //     height: 3.5,
-  //     type: 'grass'
-  // }
-  ];
+  let pokemonList = [];
   let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
   
 
